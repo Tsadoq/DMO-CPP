62930bfc6b9a1c0f27881d2285a774b4059113ab
Filippo Barba <s223491@studenti.polito.it>
2020-01-07 20:58:37 +0100

    double mutation

--- src/neighbours.cpp
+++ src/neighbours.cpp
@@ -130,18 +130,19 @@ void neighbours_by_swapping(Solution* solution, int totTimeslots){
 void neighbour_by_crossover(Solution* actual_sol,Solution* best_sol, int n_exams, int n_timeslot){
     vector<int> best_sol_ts=best_sol->timeslot_per_exams;
     vector<int> timeslot_crossover=vector<int>(n_exams, 0);
-    int first_cut=round(n_exams/4.0);
-    int second_cut=round(3.0*n_exams/4.0);
+    int first_cut=round(n_exams/5.0);
+    int second_cut=round(4.0*n_exams/5.0);
     for(int i=first_cut;i<second_cut;i++){
         timeslot_crossover[i]=best_sol->timeslot_per_exams[i];
     }
-    int exam_no_ok=0;
+    int exam_no_ok = 0;
+
     for(int j=second_cut;j<n_exams;j++){
         // gli esami nella actual_sol non sono tra loro in conflitto, devo solo controllare che non siano in conflitto con la best_sol
         if(find(best_sol->all_exams[j]->conflict_times.begin(), best_sol->all_exams[j]->conflict_times.end(),
          actual_sol->timeslot_per_exams[j]) == best_sol->all_exams[j]->conflict_times.end()){
              timeslot_crossover[j]=actual_sol->timeslot_per_exams[j];
-        }else{
+        } else {
             exam_no_ok++;
         }
     }
@@ -150,7 +151,7 @@ void neighbour_by_crossover(Solution* actual_sol,Solution* best_sol, int n_exams
         if(find(best_sol->all_exams[j]->conflict_times.begin(), best_sol->all_exams[j]->conflict_times.end(),
          actual_sol->timeslot_per_exams[j]) == best_sol->all_exams[j]->conflict_times.end()){
              timeslot_crossover[j]=actual_sol->timeslot_per_exams[j];
-        }else{
+        } else{
             exam_no_ok++;
         }
     }
@@ -171,8 +172,127 @@ void neighbour_by_crossover(Solution* actual_sol,Solution* best_sol, int n_exams
         }
 
     }
-    if(all_exam_ok<exam_no_ok){
+    if(all_exam_ok< exam_no_ok){
         actual_sol->timeslot_per_exams=best_sol_ts;
     }
     actual_sol->update_timeslots(n_exams);
+}
+
+
+void neighbours_by_double_mutation(Solution* solution, int totTimeslots, int numMutation){
+    // funzione che serve a modificare la soluzione tentando di spostare esami in timeslot a prima vista non disponibili, ma che diventano disponibili facilmente
+    // cerco esami che hanno timeslot in cui hanno un solo conflitto
+    int counter = 0;
+    int counterMutation = 0;
+    vector<int> times;
+    vector<int> possible_times;
+    vector<int> possible_timeslots;
+    // per gli spostamenti del scondo esame considerato
+    for (int i=0; i<totTimeslots;i++){
+        possible_timeslots.push_back(i+1);
+    }
+    vector<vector<int>> tabulist; // lista che tiene conto degli esami gia' spostati cosi, in modo da evitare secondo scambio
+    while (counterMutation < numMutation && counter < solution->timeslot_per_exams.size()){
+        int i = rand() % solution->timeslot_per_exams.size(); // scelgo un esame a caso
+        times = vector<int>(totTimeslots,0);
+        for (int j = 0; j < solution->all_exams[i]->conflict_times.size(); j++){
+            times[ solution->all_exams[i]->conflict_times[j]-1 ]++;
+        }
+        // in questo momento in times ci sono il numero di conflitti dell'esame i in ogni timeslot
+
+        // scelgo a caso, se c'e' un timeslot tra gli unavaiable che me lo permette
+        
+        for (int j= 0; j < totTimeslots; j++){
+            if (times[j]== 1){
+                possible_times.push_back(j); // salvo le posizioni dei timeslot che hanno un solo conflitto con l'esame i
+            }
+        }
+        // in possible times ci sono i timeslot che hanno 1 solo conflitto con l'esame i
+        if (possible_times.size() > 0){
+            cout<<"I found a timeslot where n of conflicts is 1"<<endl;
+            int selected_time = rand() % possible_times.size();
+            int conflict_index = -1;
+            // se l'esame che e' in conlitto può spostarsi, allora sposto lui e posso inserire l'esame i nel timeslot selected time
+            for (int j= 0; j <solution->all_exams[i]->conflict_times.size(); j++){
+                if (solution->all_exams[i]->conflict_times[j]== selected_time){
+                    conflict_index = j; // cerco la posizione dell'esame in conflitto 
+                }
+            }
+            vector<int> possible_mutation_for_second_exam;
+            vector<int> not_available_sec = solution->all_exams[conflict_index]->conflict_times;
+            sort(not_available_sec.begin(), not_available_sec.end()); 
+            // find available timeslot 
+            set_difference(possible_timeslots.begin(), possible_timeslots.end(), not_available_sec.begin(),  
+                            not_available_sec.end(),inserter(possible_mutation_for_second_exam, possible_mutation_for_second_exam.begin()));
+            
+            vector<int> toavoid = {i, conflict_index};
+            vector<int> toavoidreverse = {conflict_index, i};
+            if (possible_mutation_for_second_exam.size()> 0 && find(tabulist.begin(), tabulist.end(), toavoid) == tabulist.end() && find(tabulist.begin(), tabulist.end(), toavoidreverse) == tabulist.end() ){
+                // se questo swap non era gia stato fatto
+                cout<<"I'm double swapping exams: "<<i<<" and "<<conflict_index<<endl;
+                tabulist.push_back(toavoid);
+                solution->all_exams[i]->timeslot = possible_times[selected_time];
+                solution-> all_exams[conflict_index]->timeslot = possible_mutation_for_second_exam[ rand() % possible_mutation_for_second_exam.size() ];
+                counterMutation++;
+                solution->update_timeslots(solution->all_exams.size());
+                cout<<"number of mutation is "<<counterMutation<<endl;
+            }
+            possible_mutation_for_second_exam.clear();
+            not_available_sec.clear();  
+        }
+        counter++;
+        possible_times.clear();
+    }
+}
+
+
+
+
+int double_mutation(Solution* solution, int totTimeslots){
+    // funzione che serve a modificare la soluzione tentando di spostare esami in timeslot a prima vista non disponibili, ma che diventano disponibili facilmente
+    // cerco esami che hanno timeslot in cui hanno un solo conflitto
+    vector<int> times;
+    vector<int> possible_times;
+    vector<int> possible_timeslots;
+    // per gli spostamenti del scondo esame considerato
+    for (int i=0; i<totTimeslots;i++){
+        possible_timeslots.push_back(i+1);
+    }
+    int i = rand() % solution->timeslot_per_exams.size(); // scelgo un esame a caso
+    times = vector<int>(totTimeslots,0);
+    for (int j = 0; j < solution->all_exams[i]->conflict_times.size(); j++){
+        times[ solution->all_exams[i]->conflict_times[j]-1 ]++;
+    }
+    for (int j= 0; j < totTimeslots; j++){
+        if (times[j] == 1){
+            possible_times.push_back(j+1); // salvo i timeslot che hanno un solo conflitto con l'esame i (da 1 a n_timeslots)
+        }
+    }
+    // in possible times ci sono i timeslot che hanno 1 solo conflitto con l'esame i
+    if (possible_times.size() > 0){
+        int selected_time = rand() % possible_times.size();
+        int conflict_index = -1;
+        // se l'esame che e' in conlitto può spostarsi, allora sposto lui e posso inserire l'esame i nel timeslot selected time
+        for (int j= 0; j <solution->all_exams[i]->conflict_times.size(); j++){
+            if (solution->all_exams[i]->conflict_times[j]== possible_times[selected_time]){
+                conflict_index = j; // cerco la posizione dell'esame in conflitto 
+                break;
+                }
+        }
+        vector<int> possible_mutation_for_second_exam;
+        vector<int> not_available_sec = solution->all_exams[conflict_index]->conflict_times;
+        sort(not_available_sec.begin(), not_available_sec.end());  
+        set_difference(possible_timeslots.begin(), possible_timeslots.end(), not_available_sec.begin(),  
+                            not_available_sec.end(),inserter(possible_mutation_for_second_exam, possible_mutation_for_second_exam.begin()));
+        if (possible_mutation_for_second_exam.size()> 0 ){
+            // se questo swap non era gia stato fatto
+            int rand_ind = rand() % possible_mutation_for_second_exam.size();
+            solution->all_exams[i]->timeslot = possible_times[selected_time];
+            solution-> all_exams[conflict_index]->timeslot = possible_mutation_for_second_exam[rand_ind ];
+            solution->update_timeslots(solution->all_exams.size());
+            cout<<endl;
+            return 1;
+        }  
+    }
+    return 0;
 }
